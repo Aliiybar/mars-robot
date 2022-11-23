d8b63bb86d60891e89b233d9bdad8827dd05f641
Ali Iybar <ali.iybar@gmail.com>
2022-11-23 12:45:52 +0000

    First passing test

--- src/move.js
+++ src/move.js
@@ -1,3 +1,19 @@
 export default function Move(plateau, commands){
-    return "Not implemented yet"
+    const dimensions = plateau.split("x");
+    const xMax = dimensions[0];
+    const yMax = dimensions[1];
+    let currentX = 1;
+    let currentY = 1;
+    let currentDirection = "North"
+
+    const commandArray = commands.split('');
+
+    commandArray.map(cmd => {
+        switch (cmd){
+            case "F":
+                currentX += 1;
+        }
+    })
+
+    return currentX.toString() + "," + currentY.toString() + "," + currentDirection;
 }
--- src/move.test.js
+++ src/move.test.js
@@ -2,12 +2,12 @@ import Move from "./move.js"
 
 describe("Rover movement tests", () => {
   describe('Happy paths', ()=> {
-    test("Given_TheCommandAs_FF_When_ItMoves_Then_PositionShouldBe_3,1,N", () => {
+    test("Given_TheCommandAs_FF_When_ItMoves_Then_PositionShouldBe_3,1,North", () => {
       const plateau = "5x5";
       const commands = "FF"
 
       const result = Move(plateau, commands);
-      expect(result).toBe('3,1,N');
+      expect(result).toBe('3,1,North');
     })
 }) 
 })
